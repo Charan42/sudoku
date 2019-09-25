951ecb4a29fdb80d37c5a074ebe86c003052156e
charan42 <nlcgopal@gmail.com>
2019-09-25 14:35:48 +0200

    Updates

---
+++ precache-manifest.45681176885f0ec9bef53347ff02388c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1c2e90a7ab8b9c42deb",
+    "url": "/static/js/main.f1c2e90a.chunk.js"
+  },
+  {
+    "revision": "bfbadc282af6a0fe8b9d",
+    "url": "/static/js/1.bfbadc28.chunk.js"
+  },
+  {
+    "revision": "f1c2e90a7ab8b9c42deb",
+    "url": "/static/css/main.9b23c535.chunk.css"
+  },
+  {
+    "revision": "e7e656ab7c1260ce88a820d1d19f0314",
+    "url": "/index.html"
+  }
+];
