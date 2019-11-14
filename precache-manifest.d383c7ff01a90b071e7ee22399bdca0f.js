7c27c330f0384b5d2a572a7b12538929068224af
charan42 <nlcgopal@gmail.com>
2019-11-14 14:14:48 +0100

    Updates

--- precache-manifest.d383c7ff01a90b071e7ee22399bdca0f.js
+++ precache-manifest.d383c7ff01a90b071e7ee22399bdca0f.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/sudoku/static/js/runtime~main.738a16a1.js"
   },
   {
-    "revision": "e53b0ef95b9faf849af7",
-    "url": "/sudoku/static/js/main.e53b0ef9.chunk.js"
+    "revision": "76c69287b927de41838f",
+    "url": "/sudoku/static/js/main.76c69287.chunk.js"
   },
   {
     "revision": "bfbadc282af6a0fe8b9d",
     "url": "/sudoku/static/js/1.bfbadc28.chunk.js"
   },
   {
-    "revision": "e53b0ef95b9faf849af7",
-    "url": "/sudoku/static/css/main.e4e9e49a.chunk.css"
+    "revision": "76c69287b927de41838f",
+    "url": "/sudoku/static/css/main.f122ad93.chunk.css"
   },
   {
-    "revision": "a169238488e05a6988467219598b8160",
+    "revision": "785b3c8ad6b4e9e3917eec2c146807a5",
     "url": "/sudoku/index.html"
   }
 ];
